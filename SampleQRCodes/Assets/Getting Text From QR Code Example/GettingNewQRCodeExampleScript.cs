dd18230214e6dc125e13e3487a241e03fb9a0adb
v.strelchenko <v.strelchenko@holo-light.com>
2020-03-06 15:58:55 +0100

    simple example for subscribing to the QR Code Added event

---
+++ SampleQRCodes/Assets/Getting Text From QR Code Example/GettingNewQRCodeExampleScript.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.MixedReality.QR;
+using QRTracking;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GettingNewQRCodeExampleScript : MonoBehaviour
+{
+    public QRCodesManager qrCodesManager;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        qrCodesManager.QRCodeAdded += LogQRCode;
+        qrCodesManager.QRCodeAdded += AppendText;
+    }
+
+    private void AppendText(object sender, QRCodeEventArgs<Microsoft.MixedReality.QR.QRCode> e)
+    {
+        if (e != null)
+        {
+            var qrCode = e.Data;
+            text.text += $"\nNew QR code found: id = {qrCode?.Id}, text = {GetQRCodeText(qrCode)}";
+        }
+    }
+
+    private void LogQRCode(object sender, QRCodeEventArgs<Microsoft.MixedReality.QR.QRCode> e)
+    {
+        if (e != null)
+        {
+            var qrCode = e.Data;
+            Debug.Log($"New QR code found: id = {qrCode?.Id}, text = {GetQRCodeText(qrCode)}");
+        }
+    }
+
+    private string GetQRCodeText(Microsoft.MixedReality.QR.QRCode qrCode)
+    {
+        return qrCode?.Data;
+    }
+}
